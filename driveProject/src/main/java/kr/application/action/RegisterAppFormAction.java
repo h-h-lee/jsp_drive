6f6d8f7d34b33dd7e457a0175fccc7902e31d733
Hyunhee Lee <84010530+lhh3052@users.noreply.github.com>
2021-09-13 21:18:35 +0900

    수강신청 기능 구현/디자인 수정

--- driveProject/src/main/java/kr/application/action/RegisterAppFormAction.java
+++ driveProject/src/main/java/kr/application/action/RegisterAppFormAction.java
@@ -1,10 +1,12 @@
 package kr.application.action;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.application.dao.ApplicationDAO;
 import kr.controller.Action;
 import kr.course.dao.CourseDAO;
 import kr.course.vo.CourseVO;
@@ -26,17 +28,20 @@ public class RegisterAppFormAction implements Action{
 		int count = dao.getCourseCount();
 		
 		//페이지 처리
-		PaginationUtil page = new PaginationUtil(Integer.parseInt(pageNum), count, 5, 5, "listApp.do");
+		PaginationUtil page = new PaginationUtil(Integer.parseInt(pageNum), count, 5, 5, "registerAppForm.do");
 		
 		//과정 목록
 		List<CourseVO> list = null;
+		HashMap<Integer,String> map = null;
 		if(count>0) {
 			list = dao.getListCourse(page.getStartCount(),page.getEndCount());
+			map = ApplicationDAO.getinstance().getTeacherMap();
 		}
 		
 		request.setAttribute("count", count);
 		request.setAttribute("list", list);
 		request.setAttribute("pagingHtml", page.getPagingHtml());
+		request.setAttribute("map", map);
 		
 		return "/WEB-INF/views/application/registerAppForm.jsp";
 	}
--- driveProject/src/main/webapp/WEB-INF/views/application/modifyAppResult.jsp
+++ driveProject/src/main/webapp/WEB-INF/views/application/modifyAppResult.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <script type="text/javascript">
-	alert('변경이 완료되었습니다.');
-	location.href='listAllApp.do';
+	alert('신청결과 변경이 완료되었습니다.');
+	location.href='detailApp.do?app_num=${app_num}';
 </script>
